de380e2145e1e4946c7a669fd94d56b16b8c0747
ciel115 <harleypup115@aol.com>
2019-05-08 19:26:15 -0600

    generic message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'craft';
+export class AppComponent implements OnInit {
+    //recipes$: Observable<Recipe[]>;
+
+    constructor(private http:HttpClient) {
+    }
+
+    ngOnInit() {
+        console.log("test this");
+        this.http.get('https://xivapi.com/recipe/95?pretty=1').subscribe((response)=>{
+
+        console.log(response);
+        })
+    }
 }
