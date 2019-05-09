a087239ef9658013db5cec0e5b211c066d7ca595
Troyd <troyd@repercussus.com>
2019-05-08 19:22:25 -0600

    Pulling an item and displaying some relavant things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'craft';
+export class AppComponent implements OnInit  {
+  
+    selectedItem:any = {};
+    processedObject:any = {};
+
+    constructor(private http:HttpClient){}
+
+    ngOnInit(): void {
+      //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+      //Add 'implements OnInit' to the class.
+     
+      this.fetchItem(95);
+
+
+    }
+
+
+    
+    fetchItem(itemID){ //fetches the XivItem Properties for the given ID
+
+      this.http.get('https://xivapi.com/recipe/' + itemID).subscribe((response)=>{ 
+          this.selectedItem = response;
+          this.doTheCraftThingCielWants(this.selectedItem);
+      
+      });
+
+    }
+
+
+    doTheCraftThingCielWants(item)
+    {
+        this.processedObject.halfQuality = Math.floor(item.RecipeLevelTable.Quality / 2); 
+
+    }
+
 }
